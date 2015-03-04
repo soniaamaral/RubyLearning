b8d87396501dbb21826998f48174e5cb01b17197
Sonia Rohter <Sonia@Rafals-MacBook-Pro.local>
2015-03-04 22:42:41 +0100

    apply rules

--- game_of_life.rb
+++ game_of_life.rb
@@ -6,15 +6,36 @@ current_grid = [
   [ 1, 1, 0, 0 ],
   [ 1, 1, 0, 0 ] ]
 
+def count_live_neighbors x, y, grid
+  2
+end
+
 def apply_rules(grid)
-  grid
+  new_grid = Array.new(grid.size) { [] }
+
+  grid.each_index do |cell_coordinate_y|
+    row = grid[cell_coordinate_y]
+    row.each_index do |cell_coordinate_x|
+      case count_live_neighbors(cell_coordinate_x, cell_coordinate_y, grid)
+      when 0..1
+        new_grid[cell_coordinate_y][cell_coordinate_x] = 0
+      when 2
+        new_grid[cell_coordinate_y][cell_coordinate_x] = grid[cell_coordinate_y][cell_coordinate_x]
+      when 3
+        new_grid[cell_coordinate_y][cell_coordinate_x] = 1
+      else
+        new_grid[cell_coordinate_y][cell_coordinate_x] = 0
+      end
+    end
+  end
+  new_grid
 end
 
 def print_grid(grid)
   grid.each do |row|
-    row.each do |element|
-      print "@" if element==1
-      print "." if element==0
+    row.each do |cell|
+      print "@" if cell==1
+      print "." if cell==0
     end
     print "\n"
   end
