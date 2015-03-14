ea03c8fe66e5109e13179187ea9f545be12f6c17
Sonia Amaral <Sonia@Rafals-MBP.home>
2015-03-14 22:14:40 +0100

    drawing paddles

--- pong.rb
+++ pong.rb
@@ -2,8 +2,30 @@ require 'gosu'
 
 class PongWindow < Gosu::Window
   def initialize
-    super(640, 480, false)
+    @width = 640
+    @height = 480
+    super(@width, @height, false)
     self.caption = "Pong"
+
+    @background_color = Gosu::Color.rgb(0, 0, 64)
+    @paddle_color = Gosu::Color.rgb(128, 192, 255)
+    @paddle_width = 16
+    @paddle_height = 64
+    @paddle_margin = 10
+  end
+
+  def draw
+    draw_rect 0, 0, @width, @height, @background_color
+    draw_rect @paddle_margin, 100, @paddle_width, @paddle_height, @paddle_color
+    draw_rect @width - @paddle_margin - @paddle_width, 100, @paddle_width, @paddle_height, @paddle_color
+  end
+
+  def draw_rect x, y, width, height, color
+    draw_quad \
+      x, y, color,
+      x, y + height, color,
+      x + width, y + height, color,
+      x + width, y, color, 0
   end
 end
 
