0f81cc5a174770d996a9ead95a8728f62e45ec61
Sonia Amaral <Sonia@Rafals-MBP.home>
2015-03-14 22:41:19 +0100

    ball moving at constant velocity

--- pong.rb
+++ pong.rb
@@ -1,4 +1,5 @@
 require 'gosu'
+require 'vector2d'
 
 class PongWindow < Gosu::Window
   def initialize
@@ -14,13 +15,25 @@ class PongWindow < Gosu::Window
     @paddle_margin = 10
     @ball_radius = 7
     @ball_color = Gosu::Color.rgb(255, 192, 128)
+    @ball_position = Vector2d(@width / 2, @height / 2)
+    @ball_abs_velocity = 100 # px/s
+    @ball_velocity = Vector2d(rand(-100..100), rand(-100..100)).normalize * @ball_abs_velocity
   end
 
   def draw
     draw_rect 0, 0, @width, @height, @background_color
     draw_rect @paddle_margin, 100, @paddle_width, @paddle_height, @paddle_color
     draw_rect @width - @paddle_margin - @paddle_width, 100, @paddle_width, @paddle_height, @paddle_color
-    draw_rect 321, 212, @ball_radius * 2, @ball_radius * 2, @ball_color
+    draw_rect @ball_position.x - @ball_radius, @ball_position.y - @ball_radius, @ball_radius * 2, @ball_radius * 2, @ball_color
+  end
+
+  def update
+    current_time = Gosu::milliseconds
+    if not @last_time.nil?
+      delta = (current_time - @last_time) / 1000.0
+      @ball_position += @ball_velocity * delta
+    end
+    @last_time = current_time
   end
 
   def draw_rect x, y, width, height, color
