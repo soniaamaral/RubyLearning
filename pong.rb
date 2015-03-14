fb7f249293c006e3f0c69369592c2960831ff88c
Sonia Amaral <Sonia@Rafals-MBP.home>
2015-03-14 22:19:09 +0100

    drawing ball

--- pong.rb
+++ pong.rb
@@ -12,12 +12,15 @@ class PongWindow < Gosu::Window
     @paddle_width = 16
     @paddle_height = 64
     @paddle_margin = 10
+    @ball_radius = 7
+    @ball_color = Gosu::Color.rgb(255, 192, 128)
   end
 
   def draw
     draw_rect 0, 0, @width, @height, @background_color
     draw_rect @paddle_margin, 100, @paddle_width, @paddle_height, @paddle_color
     draw_rect @width - @paddle_margin - @paddle_width, 100, @paddle_width, @paddle_height, @paddle_color
+    draw_rect 321, 212, @ball_radius * 2, @ball_radius * 2, @ball_color
   end
 
   def draw_rect x, y, width, height, color
