2adfb2e09dc74824cbcea3b0600b5f44552c16f2
soniaamaral <sonia@Macinto5fb9e5d2.home>
2015-03-20 18:08:18 +0100

    file for dictionary

--- dictionary.rb
+++ dictionary.rb
@@ -3,12 +3,14 @@ word = STDIN.gets
 
 dictionary = Hash.new
 
+File.open("out_file.txt", "r")
+
 if dictionary[word] == nil
   print "What is the definition of this word?\n"
   definition = STDIN.gets
   dictionary[word] = definition
-  #definition << dictionary
-  print dictionary, "\n"
+  print "The definition of #{word} is: #{dictionary[word]}\n"
+  out_file = File.open("out_file.txt", "a") {|f| f.write(dictionary)}
 else
   print "The definition of #{word} is: #{dictionary[word]}\n"
 end
