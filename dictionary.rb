3d49730431a10638d1d2091e963e6c501524a493
soniaamaral <sonia@Macinto5fb9e5d2.home>
2015-03-21 19:46:26 +0100

    Added ability to store to file

--- dictionary.rb
+++ dictionary.rb
@@ -1,16 +1,19 @@
 print "Please enter the word you would like to look up:"
-word = STDIN.gets
+word = STDIN.gets.chomp
 
-dictionary = Hash.new
-
-File.open("out_file.txt", "r")
-
-if dictionary[word] == nil
-  print "What is the definition of this word?\n"
-  definition = STDIN.gets
-  dictionary[word] = definition
-  print "The definition of #{word} is: #{dictionary[word]}\n"
-  out_file = File.open("out_file.txt", "a") {|f| f.write(dictionary)}
+if File.exist?("dictionary.dat")
+  dictionary = Marshal.load(File.read("dictionary.dat", mode:"rb"))
 else
-  print "The definition of #{word} is: #{dictionary[word]}\n"
+  dictionary = Hash.new
 end
+
+definition = dictionary[word]
+
+if definition == nil
+  print "What is the definition of this word?\n"
+  definition = STDIN.gets.chomp
+  dictionary[word] = definition
+  File.open("dictionary.dat", "wb") {|f| f.write(Marshal.dump(dictionary))}
+end
+
+print "The definition of #{word} is: #{definition}\n"
