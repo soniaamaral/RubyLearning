d72dc4c18453b07ad6eab8a1f21226f1f316b865
soniaamaral <sonia@Macinto5fb9e5d2.home>
2015-03-21 20:53:58 +0100

    added class dictionary

--- dictionary.rb
+++ dictionary.rb
@@ -1,19 +1,34 @@
+class Dictionary
+  def initialize file_name
+    @file_name = file_name
+    @definitions = if File.exist?(file_name)
+      Marshal.load(File.read(file_name, mode:"rb"))
+    else
+      Hash.new
+    end
+  end
+
+  def get_definition_of word
+    @definitions[word]
+  end
+
+  def add_word_definition word, definition
+    @definitions[word] = definition
+    File.open(@file_name, "wb") {|f| f.write(Marshal.dump(@definitions))}
+  end
+end
+
+dictionary = Dictionary.new("dictionary.dat")
+
 print "Please enter the word you would like to look up:"
 word = STDIN.gets.chomp
 
-if File.exist?("dictionary.dat")
-  dictionary = Marshal.load(File.read("dictionary.dat", mode:"rb"))
-else
-  dictionary = Hash.new
-end
-
-definition = dictionary[word]
+definition = dictionary.get_definition_of(word)
 
 if definition == nil
   print "What is the definition of this word?\n"
   definition = STDIN.gets.chomp
-  dictionary[word] = definition
-  File.open("dictionary.dat", "wb") {|f| f.write(Marshal.dump(dictionary))}
+  dictionary.add_word_definition(word, definition)
 end
 
 print "The definition of #{word} is: #{definition}\n"
